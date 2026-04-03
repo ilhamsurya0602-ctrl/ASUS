cc42a0f6497b266610226f6a78134ed9321c7083
portofolio-rpl-hamz <ilhamsurya06022gmail.com>
2026-04-03 15:20:00 +0700

    update home section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-import Footer from "./components/Footer";
 function App() {
   return (
     <BrowserRouter>
