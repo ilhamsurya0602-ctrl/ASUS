334d9291f59edecdbc6eb6b8831d2618f95d8e01
portofolio-rpl-hamz <ilhamsurya06022gmail.com>
2026-04-03 13:42:18 +0700

    update portfolio section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,3 +1,6 @@
+import produk1 from "../assets/produk-1.jpg"
+import produk2 from "../assets/produk-2.jpg"
+import produk3 from "../assets/produk-3.jpg"
 import "./../styles/portfolio.css"; // pastikan path benar
 
 export default function Portfolio() {
@@ -14,15 +17,15 @@ export default function Portfolio() {
 
         <div className="grid">
           <div className="card">
-            <img src="src/assets/produk-1.jpg" alt="Meja Kayu Jati" />
+            <img src={produk1} alt="Meja Kayu Jati" />
             <h3>Meja Kayu Jati</h3>
           </div>
           <div className="card">
-            <img src="src/assets/produk-2.jpg" alt="Kursi Minimalis" />
+            <img src={produk2} alt="Kursi Minimalis" />
             <h3>Kursi Minimalis</h3>
           </div>
           <div className="card">
-            <img src="src/assets/produk-3.jpg" alt="Lemari Modern" />
+            <img src={produk3} alt="Lemari Modern" />
             <h3>Lemari Modern</h3>
           </div>
         </div>
